53a135986e9bb446e02a8d6143dbe0acb87f116b
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2024-04-23 15:17:31 -0700

    Update configuration

---
+++ prettier.config.js
@@ -0,0 +1,19 @@
+/** @type {import("prettier").Config} */
+const config = {
+    useTabs: false,
+    tabWidth: 4,
+    singleQuote: true,
+    trailingComma: 'all',
+    printWidth: 100,
+    plugins: ['prettier-plugin-svelte'],
+    overrides: [
+        {
+            files: '*.svelte',
+            options: {
+                parser: 'svelte',
+            },
+        },
+    ],
+};
+
+export default config;
