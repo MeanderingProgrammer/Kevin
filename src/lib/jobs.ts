e278ca2b1cf42baad8d62d9066e3c9c914c0bb52
thatthing117 <150973057+thatthing117@users.noreply.github.com>
2024-05-01 21:25:58 -0700

    a taste of the memes that we could achieve, if only Kevin had slack and access to drawIO (#1)

--- src/lib/jobs.ts
+++ src/lib/jobs.ts
@@ -8,16 +8,36 @@ export const ALL_JOBS: Job[] = [
     {
         id: '0001',
         department: 'Engineering',
-        title: 'Machine Learning Researcher',
+        title: 'Pre-Devinian Associate I',
     },
     {
         id: '0002',
         department: 'Engineering',
-        title: 'Software Engineer',
+        title: 'Pre-Devinian Associate II',
     },
     {
         id: '0003',
-        department: 'General',
-        title: 'General Application',
+        department: 'Project',
+        title: 'Kevin\'s dad',
+    },
+    {
+        id: '0004',
+        department: 'Project',
+        title: 'Kevin\'s mom',
+    },
+    {
+        id: '0005',
+        department: 'Sales',
+        title: 'Chief Ideovator',
+    },
+    {
+        id: '0006',
+        department: 'Sales',
+        title: 'Founder/CEO',
+    },
+    {
+        id: '0007',
+        department: 'Sales',
+        title: 'Graph Drawerer',
     },
 ];
